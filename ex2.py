c2ee63d65eeacc9021d2247ce4899e1f51ba87c9
kirillMIN <kirillminov@icloud.com>
2020-10-08 20:03:15 +0300

    added ex2(+ u can write your own var)

--- ex2.py
+++ ex2.py
@@ -1,6 +1,10 @@
 #change_list = [2, 'text', 456, 45.3, None, 5]
 #change_list = [2, 'text', 456, 6, 7]
-change_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+change_list = []
+number = int(input("Введите конечное число элементов:"))
+for index in range(number):
+    new_element = input()
+    change_list.append(new_element)
 print(change_list)
 for index in range(int(len(change_list) / 2)):
     change_list[2 * index], change_list[2 * index + 1] = change_list[2 * index + 1], change_list[2 * index]
