0cf65a6c9b5a5965a7a143d3ace4664030e14a0f
kirillMIN <kirillminov@icloud.com>
2020-10-12 18:54:06 +0300

    added ex3

--- ex3.py
+++ ex3.py
@@ -1,26 +1,13 @@
-number = int(input("input number:"))
-year_dict = dict(Winter=(1, 2, 12), Spring=(3, 4, 5), Summer=(6, 7, 8), Autumn=(9, 10, 11))
-for key, (v1, v2, v3) in year_dict.items():
-    if number == v1 or number == v2 or number == v3:
-        print(key)
-year_list = [[12, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]]
-flag = False
-for row in year_list:
-    if flag:
-        break
-    for elem in range(len(row)):
-        if year_list[0][elem] == number:
-            print("Winter")
-            flag = True
-        elif year_list[1][elem] == number:
-            print("Spring")
-            flag = True
-        elif year_list[2][elem] == number:
-            print("Summer")
-            flag = True
-        elif year_list[3][elem] == number:
-            print("Autumn")
-            flag = True
+def my_func(arg1, arg2, arg3):
+    if arg1 > arg2 and arg1 > arg3:
+        return arg1
+    elif arg2 > arg1 and arg2 > arg3:
+        return arg2
+    else:
+        return arg3
 
-# year_list.index(number)
 
+number1 = int(input("input first number:"))
+number2 = int(input("input second number:"))
+number3 = int(input("input third number:"))
+print("max number:", my_func(number1, number2, number3))
