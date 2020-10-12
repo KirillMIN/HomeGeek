a15b7cd4a7292b0b5bc976a6ffc02f98be0fa468
kirillMIN <kirillminov@icloud.com>
2020-10-12 18:17:37 +0300

    added ex1

--- ex1.py
+++ ex1.py
@@ -1,3 +1,13 @@
-type_list = [2, 'text', 456, 45.3, None]
-for index in range(len(type_list)):
-    print(type(type_list[index]))
+# Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
+# Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+def division(arg1, arg2):
+    if arg2 == 0:
+        return "You cant do that"
+    else:
+        division_args = arg1 / arg2
+        return division_args
+
+
+number1 = int(input("Input arg1:"))
+number2 = int(input("Input arg2:"))
+print(division(number1, number2))
