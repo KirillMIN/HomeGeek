00eb98fc504d0f7ffef2f31ecd8e7c27eb78f9e8
kirillMIN <kirillminov@icloud.com>
2020-10-12 20:33:44 +0300

    added ex5 and fixed ex2

--- ex2.py
+++ ex2.py
@@ -3,10 +3,13 @@ def my_data(**kwargs):
     return kwargs
 
 
+
 name = input('input name:')
 surname = input('input surname:')
 year = int(input('input year:'))
 city = input('input city:')
 email = input('input email:')
 telephone = input('input telephone:')
-print(my_data(name=name, surname=surname, year=year, city=city, email=email, telephone=telephone))
+my_data(name=name, surname=surname, year=year, city=city, email=email, telephone=telephone)
+
+
--- ex5.py
+++ ex5.py
@@ -1,16 +1,18 @@
-rating = [7, 5, 3, 3, 2]
-print(f"Рейтинг - {rating}")
-number = int(input("input number"))
-rating.insert(0, number)
-print(f"Рейтинг - {rating}")
-swapped = True
-while swapped:
-    swapped = False
-    for i in range(len(rating) - 1):
-        if rating[i] < rating[i + 1]:
-            rating[i], rating[i + 1] = rating[i + 1], rating[i]
-            swapped = True
-for index in range(len(rating) - 1):
-    if rating[index] == rating[index + 1]:
-        rating[index], rating[index + 1] = rating[index + 1], rating[index]
-print(f"Рейтинг - {rating}")
+def my_sum():
+    res_sum = 0
+    func = 1
+    while func != 0:
+        number = input('Input numbers or Q for quit - ').split()
+        res = 0
+        for element in range(len(number)):
+            if number[element] == 'q' or number[element] == 'Q':
+                func = 0
+                break
+            else:
+                res = res + int(number[element])
+        res_sum = res_sum + res
+        print(f'Current sum is {res_sum}')
+    print(f' final sum is {res_sum}')
+
+
+print(my_sum())
