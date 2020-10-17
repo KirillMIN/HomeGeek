95eff5388e224156873a09f0969ef2152a400a9c
kirillMIN <kirillminov@icloud.com>
2020-10-17 11:18:11 +0300

    added ex3

--- ex4.py
+++ ex4.py
@@ -1,19 +1,5 @@
-def power(x, y):
-    if x < 0 or y > 0:
-        return "You can do that"
-    elif x == 0:
-        return 0
-    else:
-        step = -1
-        speed = x
-        while step > y:
-            step = step - 1
-            # print("power:", step)
-            x = x * speed
-            # print("number:", x)
-        return f"1 / {x}"
 
 
-number1 = int(input("input x:"))
-number2 = int(input("input y:"))
-print("it is your power:", power(number1, number2))
+my_list = [0, 2, 2, 3, 4, 3, 7]
+new = [elem for elem in my_list if my_list.count(elem) == 1]
+print(new)
