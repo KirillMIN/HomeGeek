7de4bd4e7d6226e7ce27058252c21b4a56516385
kirillMIN <kirillminov@icloud.com>
2020-10-17 11:00:09 +0300

    added ex3

--- ex3.py
+++ ex3.py
@@ -1,13 +1,3 @@
-def my_func(arg1, arg2, arg3):
-    if (arg1 > arg2) and (arg2 > arg3):
-        return arg1 + arg2
-    elif (arg3 > arg2) and (arg2 > arg1):
-        return arg2 + arg3
-    else:
-        return arg3 + arg1
 
-
-number1 = int(input("input first number:"))
-number2 = int(input("input second number:"))
-number3 = int(input("input third number:"))
-print("max sum:", my_func(number1, number2, number3))
+new_list = [elem for elem in range(20, 241) if elem % 21 == 0 or elem % 20 == 0]
+print(new_list)
