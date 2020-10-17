030571eb17b75f10fd5806e8f5e81ef30f82924b
kirillMIN <kirillminov@icloud.com>
2020-10-17 12:07:07 +0300

    added ex6

--- ex6.py
+++ ex6.py
@@ -1,7 +1,22 @@
-def int_func():
-    word = input("Input words:")
-    return print(word.title())
+from itertools import count
+from itertools import cycle
 
 
+def count_func(start_number, stop_number):
+    for elem in count(start_number):
+        if elem > stop_number:
+            break
+        else:
+            print(elem)
 
-int_func()
+
+def cycle_func(my_list, iteration):
+    i = 0
+    it = cycle(my_list)
+    while i < iteration:
+        print(next(it))
+        i += 1
+
+
+count_func(start_number=int(input("enter start number: ")), stop_number=int(input("enter stop number: ")))
+cycle_func(my_list=[1, 2], iteration=int(input("enter iteration: ")))
