049b11b8f3994fbfb7b659ac95822d61bbf294dc
kirillMIN <kirillminov@icloud.com>
2020-10-20 22:14:56 +0300

    added ex3

--- ex3.py
+++ ex3.py
@@ -1,3 +1,11 @@
+with open('file.txt', 'r') as my_file:
+    salary = []
+    poorest = []
+    list = my_file.readlines()
+    for i in list:
+        i = i.split()
+        if int(i[1]) < 20000:
+            poorest.append(i[0])
+        salary.append(i[1])
+    print(f'Оклад меньше 20.000 {poorest}, средний оклад {sum(map(int, salary)) / len(salary)}')
 
-new_list = [elem for elem in range(20, 241) if elem % 21 == 0 or elem % 20 == 0]
-print(new_list)
