b939abf4287005a33cddb6cb62a03d3bc7da9f4e
kirillMIN <kirillminov@icloud.com>
2020-10-20 22:42:22 +0300

    added ex4

--- ex4.py
+++ ex4.py
@@ -1,4 +1,10 @@
+rus_language = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+new_file = []
+with open('file.txt', 'r') as file_obj1:
+    for i in file_obj1:
+        i = i.split()
+        new_file.append(rus_language[i[0]] + ' - ' + i[2])
+    print(new_file)
 
-my_list = [1, 0, 2, 2, 3, 4, 3, 7]
-new = [elem for elem in my_list if my_list.count(elem) == 1]
-print(new)
+with open('file_new.txt', 'w') as file_obj_2:
+    file_obj_2.writelines(new_file)
