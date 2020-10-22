4fa5fc59264017069f8fb147d5a69917cd2f18f5
kirillMIN <kirillminov@icloud.com>
2020-10-22 21:31:29 +0300

    added ex4

--- ex4.py
+++ ex4.py
@@ -1,10 +1,42 @@
-rus_language = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
-new_file = []
-with open('file.txt', 'r') as file_obj1:
-    for i in file_obj1:
-        i = i.split()
-        new_file.append(rus_language[i[0]] + ' - ' + i[2])
-    print(new_file)
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self. name = name
+        self. is_police = is_police
 
-with open('file_new.txt', 'w') as file_obj_2:
-    file_obj_2.writelines(new_file)
+    def show_speed(self):
+        print(f'{self.name} speed: {self.speed}')
+
+    @staticmethod
+    def go():
+        print("Car  is moving")
+
+    @staticmethod
+    def stop():
+        print("Car is standing")
+
+    @staticmethod
+    def turn(direction):
+        print(f'Car is turning: {direction}')
+
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            print('over speed')
+        print(f'{self.name} speed: {self.speed}')
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            print('over speed')
+        print(f'{self.name} speed: {self.speed}')
+
+
+Volga = TownCar(70, 'red', 'Volga', False)
+Volga.show_speed()
+Volga.stop()
+Volga.turn('left')
+Volga.go()
