8a7f029c13d2855578b7a2478f3a711025b00e14
kirillMIN <kirillminov@icloud.com>
2020-10-22 21:40:15 +0300

    added ex5

--- ex5.py
+++ ex5.py
@@ -1,9 +1,35 @@
-def summary():
-    with open('file.txt', 'w') as file_obj:
-        line = input('Enter the numbers space-separated:')
-        file_obj.writelines(line)
-        numb = line.split()
-        print(sum(map(int, numb)))
+class Stationery:
+
+    def __init__(self, title):
+        self.title = title
+
+    @staticmethod
+    def draw():
+        print('Запуск отрисовки.')
 
 
-summary()
+class Pen(Stationery):
+    @staticmethod
+    def draw():
+        print('Рисуем ручкой.')
+
+
+class Pencil(Stationery):
+    @staticmethod
+    def draw():
+        print('Рисуем карандашом.')
+
+
+class Handle(Stationery):
+    @staticmethod
+    def draw():
+        print('Рисуем маркером.')
+
+
+work_book = Stationery('book')
+work_book.draw()
+grammar_book = Pen('book')
+grammar_book.draw()
+copy_book = Handle('book')
+copy_book.draw()
+
--- ex6.py
+++
@@ -1,7 +0,0 @@
-
-subj = {}
-with open('test.txt', 'r') as file_n:
-    for line in file_n:
-        subject, lecture, practice, lab = line.split()
-        subj[subject] = int(lecture) + int(practice) + int(lab)
-    print(f'Общее количество часов по предмету - \n {subj}')
