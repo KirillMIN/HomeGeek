d87fd9b1d8f53a3014797af4a4b8b8d900824b2c
kirillMIN <kirillminov@icloud.com>
2020-10-26 21:11:59 +0300

    added ex1

--- ex1.py
+++ ex1.py
@@ -1,26 +1,25 @@
-from time import sleep
-from itertools import cycle
+
+class Matrix:
+    def __init__(self, list_of_lists):
+        self.list_of_lists = list_of_lists
+
+    def __str__(self):
+        string = ''
+        for i in self.list_of_lists:
+            for j in i:
+                string = string + str(j) + ' '
+            string = string + '\n'
+        return string
+
+    def __add__(self, other):
+        for i in range(len(self.list_of_lists)):
+            for j in range(len(self.list_of_lists[i])):
+                self.list_of_lists[i][j] = self.list_of_lists[i][j] + other.list_of_lists[i][j]
+        return Matrix(self.list_of_lists)
 
 
-class TrafficLight:
-    __color = ["red", "yellow", "green"]
-
-    @staticmethod
-    def running(number):
-        count = 0
-        for item in cycle(TrafficLight.__color):
-            print(f'Traffic light switches: {item}')
-            if item == "red":
-                sleep(7)
-            elif item == "yellow":
-                sleep(2)
-            elif item == "green":
-                sleep(5)
-            if count > number:
-                break
-            count += 1
-
-
-numb = int(input("how many:"))
-Traffic = TrafficLight()
-Traffic.running(numb)
+m_1 = Matrix([[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]])
+m_2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+print(m_1)
+print(m_2)
+print(m_1 + m_2)
