ec563c9c4bcd970c05b87de9d20dbd05d363435e
kirillMIN <kirillminov@icloud.com>
2020-10-30 17:05:42 +0300

    added ex3

--- ex3.py
+++ ex3.py
@@ -1,38 +1,19 @@
-
-class Cell:
-    def __init__(self, quantity):
-        self.quantity = quantity
-
-    def __str__(self):
-        return f'Результат: {self.quantity * "*"}'
-
-    def __add__(self, other):
-        return self.quantity + other.quantity
-
-    def __sub__(self, other):
-        return self.quantity - other.quantity if (self.quantity - other.quantity) >= 0 else print('Отрицательно')
-
-    def __mul__(self, other):
-        return int(self.quantity * other.quantity)
-
-    def __truediv__(self, other):
-        return round(self.quantity // other.quantity)
-
-    def make_order(self, cells_row):
-        row = ''
-        for i in range(int(self.quantity / cells_row)):
-            row += f'{"*" * cells_row} \n'
-        row += f'{"*" * (self.quantity % cells_row)}'
-        return print(row)
+class IntNumbers(Exception):
+    def __init__(self, txt):
+        self.txt = txt
 
 
-cells_1 = Cell(10)
-cells_2 = Cell(5)
-print(cells_1)
-print(cells_1)
-print(cells_1 + cells_2)
-print(cells_2 - cells_1)
-print(cells_1 * cells_2)
-# print(cells_1 / cells_2)
-cells_1.make_order(3)
+my_list = []
+element = 0
+while element != 3:
+    my_list[element] = my_list.insert(element, int(input('input numbers: ')))
+    element = element + 1
+try:
+    for elem in my_list:
+        if type(elem) != int:
+            raise IntNumbers("You don't input int number !")
+except IntNumbers as err:
+    print(err)
+else:
+    print(f"Your list: {my_list}")
 
