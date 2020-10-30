946143a90d217dc2b8f0654de0d5204b8be51f77
kirillMIN <kirillminov@icloud.com>
2020-10-30 16:16:26 +0300

    added ex2

--- ex2.py
+++ ex2.py
@@ -1,46 +1,19 @@
-from abc import ABC, abstractmethod
+
+class NullDivision(Exception):
+    def __init__(self, txt):
+        self.txt = txt
 
 
-class Textile(ABC):
+divider = int(input("input divider: "))
+denominator = int(input("input denominator: "))
 
-    @abstractmethod
-    def get_consumption(self):
-        pass
-
-    @abstractmethod
-    def get_param(self):
-        pass
-
-
-class Coat(Textile):
-    def __init__(self, coat_size):
-        self.coat_size = coat_size
-
-    @property
-    def get_consumption(self):
-        return f'расход на пальто: {self.coat_size / 6.5 + 0.5}'
-
-    def get_param(self):
-        return self.coat_size / 6.5 + 0.5
-
-
-class Jacket(Textile):
-    def __init__(self, jacket_size):
-        self.jacket_size = jacket_size
-
-    @property
-    def get_consumption(self):
-        return f'расход на костюм: {self.jacket_size * 2 + 0.3}'
-
-    def get_param(self):
-        return self.jacket_size * 2 + 0.3
-
-
-coat = Coat(2)
-jacket = Jacket(4)
-print(coat.get_consumption)
-print(jacket.get_consumption)
-print(coat.get_param() + jacket.get_param())
+try:
+    if denominator == 0:
+        raise NullDivision("Your added 0!")
+except NullDivision as err:
+    print(err)
+else:
+    print(f"Your result: {divider / denominator}")
 
 
 
